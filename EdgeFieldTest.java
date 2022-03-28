b4e31ac6c31422a9a6759bc52b6007aecae376e3
pjs4644 <60444005+pjs4644@users.noreply.github.com>
2022-03-28 00:16:33 -0400

    edited compile instructuons EdgeFieldTest

--- EdgeFieldTest.java
+++ EdgeFieldTest.java
@@ -2,15 +2,14 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
-
-public class EdgeFieldTest {
+public class EdgeFieldTest{
     EdgeField testObj;
 
 
 
 
 
-@Before
+    @Before
 	public void setUp() throws Exception {
 		testObj = new EdgeField("1|Peter");
 	}
